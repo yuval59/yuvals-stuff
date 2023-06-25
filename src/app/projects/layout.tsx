74c50bdf9fe49cc761bd15aacf476f02dd36c23e
yuval59 <yuvalmaron222@gmail.com>
2023-06-25 08:47:25 +0300

    Added proper page titles

--- src/app/projects/layout.tsx
+++ src/app/projects/layout.tsx
@@ -1,11 +1,10 @@
 import { Inter } from 'next/font/google'
-import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: `Yuval's stuff`,
-  description: `All my stuff`,
+  title: 'Projects',
+  description: `Yuval's projects`,
 }
 
 export default function RootLayout({
