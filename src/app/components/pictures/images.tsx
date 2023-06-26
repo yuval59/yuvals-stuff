a52973dce1a473a7e5cf13adeaa2191e97c6c890
yuval59 <yuvalmaron222@gmail.com>
2023-06-26 08:58:06 +0300

    Fixed image keys

--- src/app/components/pictures/images.tsx
+++ src/app/components/pictures/images.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 const images = [
   <Image
-    key={0}
+    key="0"
     src="/me/IMG_20191222_163239.jpg"
     alt="A selfie of Yuval in a bus stop"
     style={{ objectFit: 'contain' }}
@@ -11,7 +11,7 @@ const images = [
   />,
 
   <Image
-    key={0}
+    key="1"
     src="/me/IMG_20210514_174845.jpg"
     alt="A selfie of Yuval with his brother"
     style={{ objectFit: 'contain' }}
@@ -20,7 +20,7 @@ const images = [
   />,
 
   <Image
-    key={0}
+    key="2"
     src="/me/IMG_20210805_070640.jpg"
     alt="A selfie of yuval with his dog at a bus stop"
     style={{ objectFit: 'contain' }}
@@ -29,7 +29,7 @@ const images = [
   />,
 
   <Image
-    key={0}
+    key="3"
     src="/me/IMG_20221102_154422.jpg"
     alt="A silly picture of Yuval and a friend cooking"
     style={{ objectFit: 'contain' }}
@@ -38,7 +38,7 @@ const images = [
   />,
 
   <Image
-    key={0}
+    key="4"
     src="/me/IMG_20230131_164314.jpg"
     alt="A selfie of Yuval with two bearded dragons on his shirt"
     style={{ objectFit: 'contain' }}
@@ -47,7 +47,7 @@ const images = [
   />,
 
   <Image
-    key={0}
+    key="5"
     src="/me/IMG_20230215_210519_545.JPG"
     alt="A picture of Yuval holding his cat (she is very cute)"
     style={{ objectFit: 'contain' }}
@@ -56,7 +56,7 @@ const images = [
   />,
 
   <Image
-    key={0}
+    key="6"
     src="/me/IMG_20230220_111732.jpg"
     alt="A selfie of Yuval at a random street"
     style={{ objectFit: 'contain' }}
@@ -65,7 +65,7 @@ const images = [
   />,
 
   <Image
-    key={0}
+    key="7"
     src="/me/IMG_20230410_121531.jpg"
     alt="A picture of Yuval in an elevator, filmed through a mirror"
     style={{ objectFit: 'contain' }}
